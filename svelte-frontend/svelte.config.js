cccd30ee3843f2604293217a938f399d314748d4
William Manahan-Makley <info@willmakley.dev>
2023-04-25 21:31:51 -0400

    remove unused adapter, put back preprocessor for typescript

--- svelte-frontend/svelte.config.js
+++ svelte-frontend/svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
 			runtime: 'nodejs18.x',
