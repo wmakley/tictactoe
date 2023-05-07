197737f854b05632d7dae83fb9864311ca03f39a
William Manahan-Makley <info@willmakley.dev>
2023-05-07 13:08:42 -0400

    stress tester is now successfully playing a game!

--- stress-tester/src/main.rs
+++ stress-tester/src/main.rs
@@ -3,13 +3,16 @@ use async_tungstenite::tungstenite::Message;
 use clap::Parser;
 use futures::prelude::*;
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc;
 use tokio::sync::oneshot;
+use tokio::time::{sleep, Duration};
 
 #[derive(Debug, Parser)]
 struct Args {
     address: String,
 }
+
+static X_SCRIPT: [usize; 4] = [4, 1, 2, 8];
+static O_SCRIPT: [usize; 4] = [0, 7, 6, 3];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,85 +24,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn play_test_game(address: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, mut rx) = mpsc::channel::<TestGameMsg>(20);
+    let mut client1 = spawn_client(
+        1,
+        address.to_string(),
+        String::from("P1"),
+        String::from(""),
+        &X_SCRIPT,
+    )
+    .await?;
+    let mut client2 = spawn_client(
+        2,
+        address.to_string(),
+        String::from("P2"),
+        client1.join_token.clone(),
+        &O_SCRIPT,
+    )
+    .await?;
 
-    let client1 = spawn_client(1, address.to_string(), String::from("P1"), String::from(""), tx.clone());
-    let client2 = spawn_client(2, address.to_string(), String::from("P2"), client1.join_token().await, tx);
-
-    // assert client1 state == client2 state
-
-    // tell client1 to make a move
-
-    // assert both states changed and move happened
-
-    // could make assertions within the client as well
-
-    let (mut conn1, _) = connect_async(address).await?;
-    let msg = conn1.next().await.ok_or("didn't receive anything")??;
-    let msg: ToBrowser = serde_json::from_str(&msg.to_string())?;
-
-    let (initial_token, _initial_state) = match msg {
-        ToBrowser::JoinedGame {
-            token,
-            player_id,
-            state,
-        } => {
-            // println!("JoinedGame token: {}, player_id: {}", token, player_id);
-            assert!(token.len() > 0, "token is empty");
-            assert!(player_id == 1, "player_id is not 1");
-            assert!(state.players.len() == 1, "players.len() is not 1");
-            assert!(state.chat.len() == 1, "chat.len() is not 1");
-            (token, state)
-        }
-        _ => {
-            return Err(format!("Received unexpected message: {:?}", msg).into());
-        }
-    };
-
-    let (mut conn2, _) = connect_async(format!("{}?token={}", address, initial_token)).await?;
-    let msg = conn2.next().await.ok_or("didn't receive anything")??;
-    let msg: ToBrowser = serde_json::from_str(&msg.to_string())?;
-    match msg {
-        ToBrowser::JoinedGame {
-            token,
-            player_id,
-            state,
-        } => {
-            assert!(token == initial_token, "token does not match");
-            assert!(player_id == 2, "player_id is not 2");
-            assert!(state.players.len() == 2, "players.len() is not 2");
-            assert!(state.chat.len() == 2, "chat.len() is not 2");
-        }
-        _ => {
-            return Err(format!("Received unexpected message: {:?}", msg).into());
-        }
-    };
-
-    let msg = conn1.next().await.ok_or("didn't receive anything")??;
-    println!("next msg on conn 1: {:?}", msg);
-    // let msg: ToBrowser = serde_json::from_str(&msg.to_string())?;
-
-    let msg = conn2.next().await.ok_or("didn't receive anything")??;
-    println!("next msg on conn 2: {:?}", msg);
-
-    // let msg: ToBrowser = serde_json::from_str(&msg.to_string())?;
+    let (_h1, _h2) = futures::try_join!(client1.finished(), client2.finished())?;
+    println!("game finished");
+    // println!("p1 history: {:?}", h1);
+    // println!("p2 history: {:?}", h2);
 
     Ok(())
 }
 
-type TestGameMsg = (u8, ToBrowser);
-
 struct Client {
-    pub id: u8,
-    join_token: String,
-    player: Option<Player>,
-    server_messages: mpsc::Receiver<ToBrowser>,
+    pub id: ClientID,
+    pub join_token: String,
+    finished: Option<oneshot::Receiver<Result<Vec<State>, String>>>,
     dropped: Option<oneshot::Sender<bool>>,
 }
 
-impl Client {
-    pub async fn join_token() -> Option<String> {
+type ClientID = usize;
 
+impl Client {
+    // Wait for game to be finished.
+    pub async fn finished(&mut self) -> Result<Vec<State>, String> {
+        if let Some(rx) = self.finished.take() {
+            match rx.await {
+                Ok(r) => r,
+                Err(e) => Err(format!("{:?}", e)),
+            }
+        } else {
+            Err("Game already finished!".into())
+        }
     }
 }
 
@@ -111,14 +80,29 @@ impl Drop for Client {
     }
 }
 
-async fn spawn_client(id: u8, address: String, player_name: String, join_token: String, tx: mpsc::Sender<TestGameMsg>) -> Client {
-    let (done_tx, mut done_rx) = oneshot::channel::<bool>();
-    let (state_tx, state_rx) = mpsc::channel::<State>(100);
+async fn spawn_client(
+    id: ClientID,
+    address: String,
+    player_name: String,
+    join_token: String,
+    script: &'static [usize],
+) -> Result<Client, String> {
+    let (dropped_tx, mut dropped_rx) = oneshot::channel::<bool>();
+    let (token_tx, token_rx) = oneshot::channel::<String>();
+    let mut token_tx = Some(token_tx);
+    let (finished_tx, finished_rx) = oneshot::channel::<Result<Vec<State>, String>>();
+
+    // TODO: needs proper escaping:
+    let url = format!("{}?token={}&name={}", address, join_token, player_name);
 
     tokio::spawn(async move {
-        let (mut conn, _) = connect_async(address).await.unwrap();
+        let (mut conn, _) = connect_async(url).await.unwrap();
 
         let mut done = false;
+        let mut player: Option<Player> = None;
+        let mut my_team: char = ' ';
+        let mut state_history: Vec<State> = Vec::with_capacity(20);
+        let mut current_move: usize = 0;
         while !done {
             tokio::select! {
                 msg = conn.next() => {
@@ -129,11 +113,49 @@ async fn spawn_client(id: u8, address: String, player_name: String, join_token: 
                                     println!("conn {}: got Text: {}", id, text);
                                     let parsed: ToBrowser = serde_json::from_str(&text).unwrap();
                                     match parsed {
-                                        ToBrowser::JoinedGame { token, player_id, state } => todo!(),
-                                        ToBrowser::GameState(_) => todo!(),
+                                        ToBrowser::JoinedGame { token, player_id, state } => {
+                                            if let Some(tx) = token_tx.take() {
+                                                let _ = tx.send(token);
+                                            }
+                                            player = state.players.iter().find(|p| p.id == player_id).map(|p| p.clone());
+                                            assert!(player.is_some(), "could not find player in players vec");
+                                            my_team = player.unwrap().team;
+                                            state_history.push(state);
+                                        },
+                                        ToBrowser::GameState(state) => {
+                                            state_history.push(state);
+                                            let state = state_history.last().unwrap();
+
+                                            // check if game is over
+                                            match state.winner {
+                                                None => {
+                                                    // take a turn if the game isn't over and it's my turn
+                                                    if state.turn == my_team && current_move < script.len() {
+                                                        let msg = FromBrowser::Move {
+                                                            space: script[current_move],
+                                                        };
+                                                        current_move += 1;
+                                                        let msg = serde_json::to_string(&msg).unwrap();
+                                                        conn.send(Message::Text(msg)).await.unwrap();
+                                                    }
+                                                },
+                                                Some(EndState::Draw) => {
+                                                    println!("conn {}: game ended in draw", id);
+                                                    done = true;
+                                                },
+                                                Some(EndState::Win(team)) => {
+                                                    if team == my_team {
+                                                        println!("conn {}: game ended in win", id);
+                                                    } else {
+                                                        println!("conn {}: game ended in loss", id);
+                                                    }
+                                                    done = true;
+                                                }
+
+                                            }
+                                        }
                                         ToBrowser::Error(_) => todo!(),
                                     }
-                                    tx.send((id, parsed)).await.unwrap();
                                 }
                                 Message::Binary(_) => todo!(),
                                 Message::Ping(data) => {
@@ -158,19 +180,41 @@ async fn spawn_client(id: u8, address: String, player_name: String, join_token: 
                     }
 
                 }
-                _ = (&mut done_rx) => {
+                _ = (&mut dropped_rx) => {
+                    println!("conn {}: got done msg probably because client dropped, exiting", id);
+                    done = true;
+                }
+                _ = sleep(Duration::from_secs(10)) => {
+                    println!("conn {}: hit 10s timeout waiting for it to be my turn, exiting", id);
                     done = true;
                 }
             }
         }
 
-        println!("exiting select loop {}", id);
+        println!("conn {}: exiting", id);
+        finished_tx.send(Ok(state_history)).unwrap();
     });
-    Client {
-        id: id,
-        join_token: join_token,
-        game_state_rx: state_rx,
-        dropped: Some(done_tx),
+
+    // Wait for a join token from the server, or cancel after 5s.
+    tokio::select! {
+        token = token_rx => {
+            match token {
+                Ok(token) => {
+                    Ok(Client {
+                        id: id,
+                        join_token: token,
+                        finished: Some(finished_rx),
+                        dropped: Some(dropped_tx),
+                    })
+                },
+                Err(e) => {
+                    Err(format!("conn {}: unexpected error waiting for token for client {:?}", id, e).into())
+                }
+            }
+        }
+        _ = sleep(Duration::from_secs(5)) => {
+            return Err(format!("conn {}: hit 5s timeout waiting for token for client", id).into());
+        }
     }
 }
 
