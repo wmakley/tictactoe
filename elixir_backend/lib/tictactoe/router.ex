7d0c6f564978790692f70b5f408c159ed0f460c4
William Manahan-Makley <info@willmakley.dev>
2023-05-20 22:04:02 -0400

    add one working test, improve appearance of 404 message

--- elixir_backend/lib/tictactoe/router.ex
+++ elixir_backend/lib/tictactoe/router.ex
@@ -22,6 +22,6 @@ defmodule Tictactoe.Router do
   end
 
   match _ do
-    send_resp(conn, 404, "not found")
+    send_resp(conn, 404, "404 Not found.\n")
   end
 end
---
+++ elixir_backend/test/tictactoe_test.exs
@@ -0,0 +1,18 @@
+defmodule TictactoeTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  doctest Tictactoe
+
+  test "help text" do
+    conn = conn(:get, "/")
+    conn = Tictactoe.Router.call(conn, [])
+    assert conn.status == 200
+  end
+
+  # ignored: the documentation isn't even right
+  # test "websocket upgrade succeeds" do
+  #   conn = conn(:get, "/ws")
+  #   upgrades = Plug.Test.send_upgrades(conn)
+  #   assert {:websocket, [opt: :value]} in upgrades
+  # end
+end
