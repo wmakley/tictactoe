30e38dc8bbe52660e2e08fb55f88bad820184940
William Manahan-Makley <info@willmakley.dev>
2023-05-21 06:50:37 -0400

    Elixir WIP: implement some of the "add player" logic

--- elixir_backend/lib/tictactoe/game.ex
+++ elixir_backend/lib/tictactoe/game.ex
@@ -9,7 +9,33 @@ defmodule Tictactoe.Game do
     chat: []
   ]
 
+  alias Tictactoe.Game
+  alias Tictactoe.Player
+
   def new(id) do
-    %Tictactoe.Game{id: id}
+    %Game{id: id}
+  end
+
+  def add_player(%Game{players: players} = game, name) do
+    if length(players) == 2 do
+      {:error, "Game is full", game}
+    else
+      case List.last(players) do
+        nil ->
+          add_player(game, name, 1, "X")
+
+        %Player{id: id, team: "X"} ->
+          add_player(game, name, id + 1, "O")
+
+        %Player{id: id, team: "O"} ->
+          add_player(game, name, id + 1, "X")
+      end
+    end
+  end
+
+  defp add_player(game, name, id, team) do
+    player = %Player{id: id, name: name, team: team}
+    game = %{game | players: game.players ++ [player]}
+    {:ok, player, game}
   end
 end
--- elixir_backend/test/tictactoe.ex/game_test.exs
+++ elixir_backend/test/tictactoe.ex/game_test.exs
@@ -2,14 +2,32 @@ defmodule Tictactoe.GameTest do
   use ExUnit.Case, async: true
 
   test "new game" do
-    game = Tictactoe.Game.new("123")
-    assert game.id == "123"
+    game = Tictactoe.Game.new("test")
+    assert game.id == "test"
   end
 
   test "json encoding" do
-    game = Tictactoe.Game.new("123")
+    game = Tictactoe.Game.new("test")
 
     assert Jason.encode!(game) ==
              ~S({"board":[" "," "," "," "," "," "," "," "," "],"chat":[],"players":[],"turn":"X","winner":null})
+  end
+
+  test "add player" do
+    game = Tictactoe.Game.new("test")
+    {:ok, player, game} = Tictactoe.Game.add_player(game, "Player 1")
+    assert player == %Tictactoe.Player{id: 1, name: "Player 1", team: "X", wins: 0}
+    assert game.players == [%Tictactoe.Player{id: 1, name: "Player 1", team: "X", wins: 0}]
+
+    {:ok, player, game} = Tictactoe.Game.add_player(game, "Player 2")
+    assert player == %Tictactoe.Player{id: 2, name: "Player 2", team: "O", wins: 0}
+
+    assert game.players == [
+             %Tictactoe.Player{id: 1, name: "Player 1", team: "X", wins: 0},
+             %Tictactoe.Player{id: 2, name: "Player 2", team: "O", wins: 0}
+           ]
+
+    {:error, msg, _game} = Tictactoe.Game.add_player(game, "Player 3")
+    assert msg == "Game is full"
   end
 end
