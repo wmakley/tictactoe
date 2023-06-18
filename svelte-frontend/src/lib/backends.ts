719f7d9427eb180db20c45ac2d2f0462040b0f93
William Manahan-Makley <info@willmakley.dev>
2023-06-18 08:03:14 -0400

    add elixir backend now that it is working

--- svelte-frontend/src/lib/backends.ts
+++ svelte-frontend/src/lib/backends.ts
@@ -8,6 +8,7 @@ export const backends: Backend[] = [
     { slug: "localhost", label: "Localhost", url: "ws://localhost:3000" },
     { slug: "rust", label: "Rust", url: "wss://tictactoe-rust-backend.fly.dev" },
     { slug: "go", label: "Go", url: "wss://tictactoe-go-backend.fly.dev" },
+    { slug: "elixir", label: "Elixir", url: "wss://tictactoe-elixir-backend.fly.dev" }
 ];
 
 export function findBackend(slug: string) {
