ca26e8568a417f3341b545a79cdd53b55f67cc63
William Manahan-Makley <info@willmakley.dev>
2023-10-01 15:28:00 -0400

    LiveView: add site.webmanifest, whatever that does

--- tictactoe_live/lib/tictactoe_live_web.ex
+++ tictactoe_live/lib/tictactoe_live_web.ex
@@ -18,7 +18,7 @@ defmodule TictactoeLiveWeb do
   """
 
   def static_paths do
-    ~w(assets fonts images favicon.ico robots.txt android-chrome-192x192.png android-chrome-512x512.png apple-touch-icon.png favicon-16x16.png favicon-32x32.png)
+    ~w(assets fonts images favicon.ico robots.txt android-chrome-192x192.png android-chrome-512x512.png apple-touch-icon.png favicon-16x16.png favicon-32x32.png site.webmanifest)
   end
 
   def router do
